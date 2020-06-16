4125e246e11a3280599bef85c75dffabdcfc482c
SeraphyBR <luisjuniorbr@gmail.com>
2020-06-16 09:54:06 -0300

    Update

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 
-const routes = express.Router();
+import PointsController from './controllers/points_controller';
+import ItemsController from './controllers/items_controller';
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello world' });
-});
+const routes = express.Router();
+const points_controller = new PointsController();
+const items_controller = new ItemsController();
+
+// padrão de nomes (comunidade) para métodos na controller
+// index (listar muitos registros) , show (mostrar apenas um registro), create, update, delete
+
+routes.get('/items', items_controller.index);
+
+routes.get('/points', points_controller.index);
+routes.get('/points/:id', points_controller.show);
+routes.post('/points', points_controller.create);
 
 export default routes;
